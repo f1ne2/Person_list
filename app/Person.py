ca05438b84dab1b77be813cf1b2aedb33906df59
f1ne2 <alextomilovski@gmail.com>
2020-12-22 02:16:58 +0300

    21.12.2020

---
+++ app/Person.py
@@ -0,0 +1,8 @@
+class Person:
+
+    def __init__(self, name=None, address=None, phone=None,
+                 identification=None):
+        self.name = name
+        self.address = address
+        self.phone = phone
+        self.id = identification
