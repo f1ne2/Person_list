6642390f5b2b7c871e43925e2700663d333297a7
f1ne2 <alextomilovski@gmail.com>
2020-12-22 02:12:26 +0300

    21.12.2020

---
+++ main.py
@@ -0,0 +1,14 @@
+from app.main_screen import *
+
+
+def main():
+    window = main_screen()
+    button_add(window)
+    button_search(window)
+    button_view(window)
+    button_exit(window)
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
