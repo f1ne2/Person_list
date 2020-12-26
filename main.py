6fbe014725bcb18d3f72e4d103a42a65d2ee18c1
f1ne2 <alextomilovski@gmail.com>
2020-12-26 15:28:24 +0300

    26.12.2020

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from app.view import *
 
 
+class Flag:
+
+    def __init__(self, flag):
+        self.__flag = flag
+
+    def define(self):
+        if self.__flag == "1":
+            return 1
+        else:
+            return 2
+
+
 def main():
+    flag = Flag(input(""))
     window = main_screen()
-    button_add(window)
-    button_search(window)
-    button_view(window)
+    button_add(window, flag.define())
+    button_search(window, flag.define())
+    button_view(window, flag.define())
     button_exit(window)
     window.mainloop()
 
 
+print("Press 1 if you would like to work with file or press any another key, "
+      "if you would you like work with database")
 if __name__ == "__main__":
     main()
