235716e148f10c88f14440054d71095eba6cbf49
f1ne2 <alextomilovski@gmail.com>
2020-12-27 21:25:16 +0300

    database 27.12.2020

--- app/database.py
+++ app/database.py
@@ -1,41 +1,56 @@
 import sqlite3
 from typing import List
 from Person import Person
+from abstract import FormalParserInterface
 
 
-def insert_into_table(new_contact_info: Person):
-    cursor.execute("""CREATE TABLE IF NOT EXISTS contacts (name text, 
-    address text, phone text, id text)""")
-    cursor.execute("""INSERT INTO contacts (name, address, phone, id) 
-    VALUES (?, ?, ?, ?)""", (new_contact_info.name, new_contact_info.address,
-                             new_contact_info.phone, new_contact_info.id))
-    conn.commit()
+class WorkWithDB(FormalParserInterface):
+    def insert_into_table(self, new_contact_info: Person):
+        cursor.execute("""CREATE TABLE IF NOT EXISTS contacts (name text, 
+        address text, phone text, id text)""")
+        cursor.execute("""INSERT INTO contacts (name, address, phone, id) 
+        VALUES (?, ?, ?, ?)""", [new_contact_info.name,
+                                 new_contact_info.address,
+                                 new_contact_info.phone, new_contact_info.id])
+        conn.commit()
 
+    def delete_from_table(self, search_id_str: str):
+        cursor.execute("DELETE FROM contacts WHERE id = ?", (search_id_str,))
+        conn.commit()
 
-def delete_from_table(search_id_str: str):
-    cursor.execute("DELETE FROM contacts WHERE id = ?", (search_id_str,))
-    conn.commit()
+    def update_table(self, new_contact_info: Person, contact_id: str):
+        cursor.execute("""UPDATE contacts SET name = ?, address = ?, phone = ?, 
+        id = ? WHERE id = ?""", [new_contact_info.name,
+                                 new_contact_info.address,
+                                 new_contact_info.phone, new_contact_info.id,
+                                 contact_id])
+        conn.commit()
 
+    def get_all_contacts(self) -> List[Person]:
+        cursor.execute("""SELECT * FROM contacts""")
+        out_info = Person()
+        out_list = []
+        tupl = cursor.fetchall()
+        for i in range(len(tupl)):
+            out_info.name = tupl[i][0]
+            out_info.address = tupl[i][1]
+            out_info.phone = tupl[i][2]
+            out_info.id = tupl[i][3]
+            out_list.append(out_info)
+            out_info = Person()
+        return out_list
 
-def update_table(new_contact_info: Person, contact_id: str):
-    cursor.execute("""UPDATE contacts SET name = ?, address = ?, phone = ?, 
-    id = ? WHERE id = ?""", (new_contact_info.name, new_contact_info.address,
-                             new_contact_info.phone, new_contact_info.id,
-                             contact_id))
-    conn.commit()
-
-
-def load_sql_database() -> List[tuple]:
-    cursor.execute("""SELECT * FROM contacts""")
-    return cursor.fetchall()
-
-
-def select_from_db(search_id_str: str) -> List[tuple]:
-    sql = "SELECT * FROM contacts WHERE id=?"
-    cursor.execute(sql, [search_id_str])
-    return cursor.fetchall()
+    def select_from_db(self, search_id_str: str) -> Person:
+        sql = "SELECT * FROM contacts WHERE id=?"
+        cursor.execute(sql, [search_id_str])
+        tupl = cursor.fetchall()
+        find_info = Person()
+        find_info.name = tupl[0][0]
+        find_info.address = tupl[0][1]
+        find_info.phone = tupl[0][2]
+        find_info.id = tupl[0][3]
+        return find_info
 
 
 conn = sqlite3.connect("my_database.db")
 cursor = conn.cursor()
-
