bc784e4d64d9fb8b9709821664333ee6ad063357
f1ne2 <alextomilovski@gmail.com>
2020-12-28 20:33:25 +0300

    controller 28.12.2020

--- app/controller.py
+++ app/controller.py
@@ -1,65 +1,35 @@
-from view import *
-from database import *
 from Model import *
 from Person import Person
+from typing import List
 
 
-def find_contact(search_id_str: str) -> Person:
-    find = WorkWithFile()
-    return find.select_from_db(search_id_str)
+class Controller:
+    def __init__(self, storage: FormalParserInterface):
+        self.storage = storage
 
+    def find_contact(self, search_id_str: str) -> Person:
+        return self.storage.select_from_db(search_id_str)
 
-def print_contacts() -> List[Person]:
-    prnt = WorkWithFile()
-    return prnt.get_all_contacts()
+    def del_contact(self, search_id_str: str):
+        self.storage.delete_from_table(search_id_str)
 
+    def load_for_add(self, new_contact_info: Person):
+        self.storage.insert_into_table(new_contact_info)
 
-def del_contact_sql_db(search_id_str: str):
-    delete = WorkWithDB()
-    delete.delete_from_table(search_id_str)
+    def load_for_edit(self, new_contact_info: Person, contact_id: str):
+        self.storage.update_table(new_contact_info, contact_id)
 
+    def load_to_view(self) -> List[Person]:
+        return self.storage.get_all_contacts()
 
-def del_contact(search_id_str: str):
-    delete = WorkWithFile()
-    delete.delete_from_table(search_id_str)
+    def edt_contact(self, edit_id_str: str, info: Person):
+        list_contacts = self.storage.get_all_contacts()
+        self.edit_contact(edit_id_str, info, list_contacts)
 
-
-def edt_contact(edit_id_str: str, info: Person):
-    edt = WorkWithFile()
-    list_contacts = edt.get_all_contacts()
-    edit_contact(edit_id_str, info, list_contacts)
-
-
-def load_for_add(new_contact_info: Person):
-    load_add = WorkWithFile()
-    load_add.insert_into_table(new_contact_info)
-
-
-def load_for_edit(new_contact_info: Person, contact_id: str):
-    load_edit = WorkWithFile()
-    load_edit.update_table(new_contact_info, contact_id)
-
-
-def load_for_add_db(new_contact_info: Person):
-    load_add_db = WorkWithDB()
-    load_add_db.insert_into_table(new_contact_info)
-
-
-def load_for_edit_db(new_contact_info: Person, contact_id: str):
-    load_edit_db = WorkWithDB()
-    load_edit_db.update_table(new_contact_info, contact_id)
-
-
-def load_from_db_to_view() -> List[Person]:
-    load_from_db = WorkWithDB()
-    return load_from_db.get_all_contacts()
-
-
-def edt_contact_sql_db(edit_id_str: str, info: Person):
-    list_contacts = load_from_db_to_view()
-    edit_contact(edit_id_str, info, list_contacts)
-
-
-def find_contact_in_db(search_id_str: str) -> Person:
-    select_db = WorkWithDB()
-    return select_db.select_from_db(search_id_str)
+    def edit_contact(self, edit_id_str: str, edit: Person, list_contacts: list):
+        for i in range(len(list_contacts)):
+            if edit_id_str == list_contacts[i].id:
+                edit.name.insert("end", list_contacts[i].name)
+                edit.address.insert("end", list_contacts[i].address)
+                edit.phone.insert("end", list_contacts[i].phone)
+                edit.id.insert("end", list_contacts[i].id)
