d71242b34e08353de9d00b2d50ae762e572b6dd1
f1ne2 <alextomilovski@gmail.com>
2021-01-03 15:19:34 +0300

    03.01.2021 Person

--- app/Person.py
+++ app/Person.py
@@ -1,8 +1,17 @@
+from typing import Optional
+
+
 class Person:
 
-    def __init__(self, name=None, address=None, phone=None,
-                 identification=None):
+    def __init__(self, name: Optional[str], address: Optional[str],
+                 phone: Optional[str], identification: Optional[str]):
         self.name = name
         self.address = address
         self.phone = phone
         self.id = identification
+
+    def __eq__(self, other):
+        if self.name == other.name and self.address == other.address and \
+                self.phone == other.phone and self.id == other.id:
+            return True
+        return False
