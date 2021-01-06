c0755aa79c26455652183452d1ef29570a4f8dc1
f1ne2 <alextomilovski@gmail.com>
2021-01-06 15:35:45 +0300

    06.01

--- tests/test_database.py
+++ tests/test_database.py
@@ -6,11 +6,13 @@ class TestDatabase(unittest.TestCase):
     def setUp(self):
         self.person_info = Person("Alex", "T", "+375", "22")
         self.sql = WorkWithDB()
+        self.conn = sqlite3.connect("my_database.db")
+        self.cursor = self.conn.cursor()
 
     def test_insert_into_table(self):
         self.sql.insert_into_table(self.person_info)
-        cursor.execute("SELECT * FROM contacts WHERE id=22")
-        cursor_out = cursor.fetchall()
+        self.cursor.execute("SELECT * FROM contacts WHERE id=22")
+        cursor_out = self.cursor.fetchall()
         self.assertEqual(cursor_out, [('Alex', 'T', '+375',
                                        '22')])
 
@@ -18,15 +20,15 @@ class TestDatabase(unittest.TestCase):
         self.sql.insert_into_table(self.person_info)
         new_person_info = Person('Olga', 'Minsk', '+375', '22')
         self.sql.update_table(new_person_info, '22')
-        cursor.execute("SELECT * FROM contacts WHERE id=22")
-        cursor_out = cursor.fetchone()
+        self.cursor.execute("SELECT * FROM contacts WHERE id=22")
+        cursor_out = self.cursor.fetchone()
         self.assertEqual(cursor_out, ('Olga', 'Minsk', '+375', '22'))
         self.sql.delete_from_table('22')
 
     def test_delete_from_table(self):
-        self.res = cursor.execute("SELECT * FROM contacts WHERE id=33")
-        conn.commit()
-        cursor_out = cursor.fetchall()
+        self.res = self.cursor.execute("SELECT * FROM contacts WHERE id=33")
+        self.conn.commit()
+        cursor_out = self.cursor.fetchall()
         self.assertEqual(cursor_out, [])
 
     def test_select_from_db(self):
@@ -43,7 +45,7 @@ class TestDatabase(unittest.TestCase):
 
     def test_create_table(self):
         self.sql.create_table()
-        res = cursor.execute("SELECT * FROM contacts")
+        res = self.cursor.execute("SELECT * FROM contacts")
         res = res.fetchall()
         self.assertEqual(res, [])
 
