442c1ae47a0031161ecd149293ec0f9f5f5a61cf
f1ne2 <alextomilovski@gmail.com>
2021-01-06 14:55:44 +0300

    06.01

--- app/database.py
+++ app/database.py
@@ -1,57 +1,42 @@
 import sqlite3
+import os
 from typing import List
 from Person import Person
 from abstract import FormalParserInterface
 
 
 class WorkWithDB(FormalParserInterface):
-    def insert_into_table(self, new_contact_info: Person) -> int:
-        sql = "SELECT * FROM contacts"
-        cursor.execute(sql)
-        tupl = cursor.fetchall()
-        if tupl != [()]:
-            for i in range(len(tupl)):
-                if new_contact_info.id == tupl[i][3]:
-                    return 2
-        cursor.execute("""INSERT INTO contacts (name, address, phone, id) 
+    def __init__(self):
+        self.file_path = "my_database.db"
+        self.conn = sqlite3.connect("my_database.db")
+        self.cursor = self.conn.cursor()
+
+    def insert_into_table(self, new_contact_info: Person) -> None:
+        self.cursor.execute("""INSERT INTO contacts (name, address, phone, id) 
         VALUES (?, ?, ?, ?)""", [new_contact_info.name,
                                  new_contact_info.address,
                                  new_contact_info.phone, new_contact_info.id])
-        conn.commit()
-        return 1
+        self.conn.commit()
 
-    def delete_from_table(self, search_id_str: str) -> int:
-        sql = "SELECT * FROM contacts"
-        cursor.execute(sql)
-        tupl = cursor.fetchall()
-        for i in range(len(tupl)):
-            if search_id_str == tupl[i][3]:
-                cursor.execute("DELETE FROM contacts WHERE id = ?",
-                               (search_id_str,))
-                conn.commit()
-                return 1
-        return 2
+    def delete_from_table(self, search_id_str: str) -> None:
+        self.cursor.execute("DELETE FROM contacts WHERE id = ?",
+                            (search_id_str,))
+        self.conn.commit()
 
-    def update_table(self, new_contact_info: Person, contact_id: str) -> int:
-        i = 0
-        if contact_id == new_contact_info.id:
-            i = 1
-            cursor.execute("""UPDATE contacts SET name = ?, address = ?, 
-            phone = ?, id = ? WHERE id = ?""", [new_contact_info.name,
-                                                new_contact_info.address,
-                                                new_contact_info.phone,
-                                                new_contact_info.id,
-                                                contact_id])
-            conn.commit()
-        if i == 1:
-            return 1
-        return 2
+    def update_table(self, new_contact_info: Person, contact_id: str) -> None:
+        self.cursor.execute("""UPDATE contacts SET name = ?, address = ?, 
+        phone = ?, id = ? WHERE id = ?""", [new_contact_info.name,
+                                            new_contact_info.address,
+                                            new_contact_info.phone,
+                                            new_contact_info.id,
+                                            contact_id])
+        self.conn.commit()
 
     def get_all_contacts(self) -> List[Person]:
-        cursor.execute("""SELECT * FROM contacts""")
+        self.cursor.execute("""SELECT * FROM contacts""")
         out_info = Person("", "", "", "")
         out_list = []
-        tupl = cursor.fetchall()
+        tupl = self.cursor.fetchall()
         for i in range(len(tupl)):
             out_info.name = tupl[i][0]
             out_info.address = tupl[i][1]
@@ -61,25 +46,23 @@ class WorkWithDB(FormalParserInterface):
             out_info = Person("", "", "", "")
         return out_list
 
-    def select_from_db(self, search_id_str: str) -> Person:
+    def select_from_db(self, search_id_str: str) -> List[tuple]:
         sql = "SELECT * FROM contacts WHERE id=?"
-        cursor.execute(sql, [search_id_str])
-        tupl = cursor.fetchall()
-        find_info = Person("", "", "", "")
-        try:
-            find_info.name = tupl[0][0]
-            find_info.address = tupl[0][1]
-            find_info.phone = tupl[0][2]
-            find_info.id = tupl[0][3]
-        except:
-            find_info = Person("", "", "", "")
-            return find_info
-        return find_info
+        self.cursor.execute(sql, [search_id_str])
+        return self.cursor.fetchall()
 
     def create_table(self) -> None:
-        cursor.execute("""CREATE TABLE IF NOT EXISTS contacts (name text, 
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS contacts (name text, 
                address text, phone text, id text)""")
 
+    def checking_id(self, new_contact_id: str) -> bool:
+        self.cursor.execute("SELECT * FROM contacts WHERE id=?",
+                            [new_contact_id])
+        if not self.cursor.fetchall():
+            return False
+        return True
 
-conn = sqlite3.connect("my_database.db")
-cursor = conn.cursor()
+    def check_file_exist(self):
+        if not os.path.exists(self.file_path):
+            print("DB not found")
+            quit()
